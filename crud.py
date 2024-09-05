81800a35b7aeccccdba55ca6c1ae4d3da14d6621
dni ⚡ <office@dnilabs.com>
2024-09-05 15:39:11 +0200

    feat: update to lnbits 1.0.0

--- crud.py
+++ crud.py
@@ -3,6 +3,8 @@ import time
 from typing import List, Optional, Union
 from uuid import uuid4
 
+from lnbits.helpers import update_query
+
 from .db import db
 from .helpers import generate_bleskomat_lnurl_hash
 from .models import Bleskomat, BleskomatLnurl, CreateBleskomat
@@ -20,19 +22,20 @@ async def create_bleskomat(data: CreateBleskomat, wallet_id: str) -> Bleskomat:
             id, wallet, api_key_id, api_key_secret, api_key_encoding,
             name, fiat_currency, exchange_rate_provider, fee
         )
-        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+        VALUES (:id, :wallet, :api_key_id, :api_key_secret, :api_key_encoding,
+                :name, :fiat_currency, :exchange_rate_provider, :fee)
         """,
-        (
-            bleskomat_id,
-            wallet_id,
-            api_key_id,
-            api_key_secret,
-            api_key_encoding,
-            data.name,
-            data.fiat_currency,
-            data.exchange_rate_provider,
-            data.fee,
-        ),
+        {
+            "id": bleskomat_id,
+            "wallet": wallet_id,
+            "api_key_id": api_key_id,
+            "api_key_secret": api_key_secret,
+            "api_key_encoding": api_key_encoding,
+            "name": data.name,
+            "fiat_currency": data.fiat_currency,
+            "exchange_rate_provider": data.exchange_rate_provider,
+            "fee": data.fee,
+        },
     )
     bleskomat = await get_bleskomat(bleskomat_id)
     assert bleskomat, "Newly created bleskomat couldn't be retrieved"
@@ -41,14 +44,14 @@ async def create_bleskomat(data: CreateBleskomat, wallet_id: str) -> Bleskomat:
 
 async def get_bleskomat(bleskomat_id: str) -> Optional[Bleskomat]:
     row = await db.fetchone(
-        "SELECT * FROM bleskomat.bleskomats WHERE id = ?", (bleskomat_id,)
+        "SELECT * FROM bleskomat.bleskomats WHERE id = :id", {"id": bleskomat_id}
     )
     return Bleskomat(**row) if row else None
 
 
 async def get_bleskomat_by_api_key_id(api_key_id: str) -> Optional[Bleskomat]:
     row = await db.fetchone(
-        "SELECT * FROM bleskomat.bleskomats WHERE api_key_id = ?", (api_key_id,)
+        "SELECT * FROM bleskomat.bleskomats WHERE api_key_id = :id", {"id": api_key_id}
     )
     return Bleskomat(**row) if row else None
 
@@ -56,27 +59,29 @@ async def get_bleskomat_by_api_key_id(api_key_id: str) -> Optional[Bleskomat]:
 async def get_bleskomats(wallet_ids: Union[str, List[str]]) -> List[Bleskomat]:
     if isinstance(wallet_ids, str):
         wallet_ids = [wallet_ids]
-    q = ",".join(["?"] * len(wallet_ids))
+    q = ",".join([f"'{wallet_id}'" for wallet_id in wallet_ids])
     rows = await db.fetchall(
-        f"SELECT * FROM bleskomat.bleskomats WHERE wallet IN ({q})", (*wallet_ids,)
+        f"SELECT * FROM bleskomat.bleskomats WHERE wallet IN ({q})"
     )
     return [Bleskomat(**row) for row in rows]
 
 
-async def update_bleskomat(bleskomat_id: str, **kwargs) -> Optional[Bleskomat]:
-    q = ", ".join([f"{field[0]} = ?" for field in kwargs.items()])
+async def update_bleskomat(bleskomat_id: str, data: CreateBleskomat) -> Bleskomat:
     await db.execute(
-        f"UPDATE bleskomat.bleskomats SET {q} WHERE id = ?",
-        (*kwargs.values(), bleskomat_id),
+        update_query("bleskomat.bleskomats", data),
+        {"id": bleskomat_id, **data.dict()}
     )
     row = await db.fetchone(
-        "SELECT * FROM bleskomat.bleskomats WHERE id = ?", (bleskomat_id,)
+        "SELECT * FROM bleskomat.bleskomats WHERE id = :id", {"id": bleskomat_id}
     )
-    return Bleskomat(**row) if row else None
+    assert row, "Bleskomat not found after update"
+    return Bleskomat(**row)
 
 
 async def delete_bleskomat(bleskomat_id: str) -> None:
-    await db.execute("DELETE FROM bleskomat.bleskomats WHERE id = ?", (bleskomat_id,))
+    await db.execute(
+        "DELETE FROM bleskomat.bleskomats WHERE id = :id", {"id": bleskomat_id}
+    )
 
 
 async def create_bleskomat_lnurl(
@@ -91,21 +96,22 @@ async def create_bleskomat_lnurl(
             id, bleskomat, wallet, hash, tag, params, api_key_id,
             initial_uses, remaining_uses, created_time, updated_time
         )
-        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+        VALUES (:id, :bleskomat, :wallet, :hash, :tag, :params, :api_key_id,
+                :initial_uses, :remaining_uses, :created_time, :updated_time)
         """,
-        (
-            bleskomat_lnurl_id,
-            bleskomat.id,
-            bleskomat.wallet,
-            lnurl_hash,
-            tag,
-            params,
-            bleskomat.api_key_id,
-            uses,
-            uses,
-            now,
-            now,
-        ),
+        {
+            "id": bleskomat_lnurl_id,
+            "bleskomat": bleskomat.id,
+            "wallet": bleskomat.wallet,
+            "hash": lnurl_hash,
+            "tag": tag,
+            "params": params,
+            "api_key_id": bleskomat.api_key_id,
+            "initial_uses": uses,
+            "remaining_uses": uses,
+            "created_time": now,
+            "updated_time": now,
+        },
     )
     bleskomat_lnurl = await get_bleskomat_lnurl(secret)
     assert bleskomat_lnurl, "Newly created bleskomat LNURL couldn't be retrieved"
@@ -115,6 +121,7 @@ async def create_bleskomat_lnurl(
 async def get_bleskomat_lnurl(secret: str) -> Optional[BleskomatLnurl]:
     lnurl_hash = generate_bleskomat_lnurl_hash(secret)
     row = await db.fetchone(
-        "SELECT * FROM bleskomat.bleskomat_lnurls WHERE hash = ?", (lnurl_hash,)
+        "SELECT * FROM bleskomat.bleskomat_lnurls WHERE hash = :hash",
+        {"hash": lnurl_hash},
     )
     return BleskomatLnurl(**row) if row else None
